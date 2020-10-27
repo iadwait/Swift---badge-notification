e3bfc924ff112b73318c9ab55e3cecf6455038c0
iAdwait <adwaitbarkale@gmail.com>
2020-10-27 14:12:30 +0530

    Initial Commit

---
+++ badge notification/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  badge notification
+//
+//  Created by Adwait Barkale on 27/10/20.
+//  Copyright © 2020 Adwait Barkale. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class ViewController: UIViewController {
+
+    var badgeNumber:Int = 10
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setBadgeNotification()
+    }
+    
+    func setBadgeNotification()
+    {
+        let application = UIApplication.shared
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.badge,.alert,.sound]) { (success, error) in
+            print("Success")
+        }
+        application.applicationIconBadgeNumber = badgeNumber
+        application.registerForRemoteNotifications()
+    }
+
+}
+
